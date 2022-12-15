99ca82d91cb8ab338b9746cf7e986c8ab09e4027
SarahSoutoul <sarahsoutoul.uk@gmail.com>
2022-12-15 10:26:07 +0000

    Final updates

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,17 +1,9 @@
 function TodoItem({ todo, deleteTodo, completeTodo }) {
-	function deleteHandler() {
-		deleteTodo(todo);
-	}
-
-	function completeHandler() {
-  		completeTodo(todo);
-	}
-
 	return(
 		<li className="todo">
 			<span className={`todo-item ${todo.completed ? 'completed' : ''}`}>{todo.text}</span>
-			<button className="trash-btn" onClick={deleteHandler}>Delete</button>
-			<button className="complete-btn" onClick={completeHandler}>Complete</button>
+			<button className="trash-btn" onClick={() => deleteTodo(todo)}>Delete</button>
+			<button className="complete-btn" onClick={() => completeTodo(todo)}>Complete</button>
 		</li>
 	)
 	}
